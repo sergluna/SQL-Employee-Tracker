51136346b479e655db3de449cfa198184d8fd4a3
Moon Man <111019074+sergluna@users.noreply.github.com>
2023-01-12 22:14:17 -0600

    fixing db connection

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,6 @@
-DROP DATABASE IF EXISTS employee_tracker_db;
-CREATE DATABASE employee_tracker_db;
-USE employee_tracker_db;
+-- Drops the employees_db if it exists currently --
+DROP DATABASE IF EXISTS employees_db;
+-- Creates the employees database --
+CREATE DATABASE employees_db;
+-- Uses the employees database --
+USE employees_db;
